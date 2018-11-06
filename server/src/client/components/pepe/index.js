148431138693c35634575c19befa9f6cede69754
Marc García Lopez <marc.garcia@marfeel.com>
2018-11-06 17:38:40 +0100

    add listener

--- server/src/client/components/pepe/index.js
+++ server/src/client/components/pepe/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { increment } from '../../actions';
 import { connect } from 'react-redux';
 
-const Pepe = () => {
+const Pepe = props => {
 	return (
 		<div id="pepe">
 			<h1>I'm pepe component, yeaaaaah!</h1>
+			<button onClick={props.increment}>increment</button>
 			<hr/>
 		</div>
 	);

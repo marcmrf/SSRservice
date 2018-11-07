ab1f049a2e05c85e4f773864fc74388705599e49
Marc García Lopez <marc.garcia@marfeel.com>
2018-11-07 16:26:53 +0100

    Revert "css modules"
    This reverts commit 0071e5cdc2b9572fc1312ce4b0e015f3d1ffeab3.

--- server/src/client/components/pepe/index.js
+++ server/src/client/components/pepe/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { increment } from '../../actions';
 import { connect } from 'react-redux';
-import './index.css';
 
 const Pepe = props => {
 	return (
-		<div id="pepe" styleName="paco">
+		<div id="pepe">
 			<h1>I'm pepe component, yeaaaaah!</h1>
 			<button onClick={props.increment}>increment</button>
 			<hr/>

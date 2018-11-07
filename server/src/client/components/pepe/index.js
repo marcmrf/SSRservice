0071e5cdc2b9572fc1312ce4b0e015f3d1ffeab3
Marc García Lopez <marc.garcia@marfeel.com>
2018-11-07 16:20:17 +0100

    css modules

--- server/src/client/components/pepe/index.js
+++ server/src/client/components/pepe/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { increment } from '../../actions';
 import { connect } from 'react-redux';
+import './index.css';
 
 const Pepe = props => {
 	return (
-		<div id="pepe">
+		<div id="pepe" styleName="paco">
 			<h1>I'm pepe component, yeaaaaah!</h1>
 			<button onClick={props.increment}>increment</button>
 			<hr/>
--- server/webpack.base.js
+++ server/webpack.base.js
@@ -1,19 +1,49 @@
 module.exports = {
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }]
-          ]
-        }
-      }
-    ]
-  }
+	// Tell webpack to run babel on every file it runs through
+	module: {
+		rules: [{
+				test: /\.js?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				options: {
+					presets: [
+						'react',
+						'stage-0',
+						['env', {
+							targets: {
+								browsers: ['last 2 versions']
+							}
+						}]
+					],
+					"plugins": [
+						["react-css-modules", {
+							"filetypes": {
+								".scss": {
+									"syntax": "postcss-scss",
+									"plugins": [
+										"postcss-nested"
+									]
+								}
+							}
+						}]
+					]
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							importLoader: 1,
+							modules: true,
+							localIdentName: '[name]_[local]__[hash:base64:5]'
+						}
+					}
+				]
+			}
+		]
+	}
 };
